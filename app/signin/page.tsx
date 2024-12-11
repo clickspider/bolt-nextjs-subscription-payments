3626326f7252f69045ebd11f785eaee4a81fd13a
Daniel Frey <danielfrey101@gmail.com>
2024-12-11 15:08:16 +0100

    feat(supabase): init setup with auth

---
+++ app/signin/page.tsx
@@ -0,0 +1,12 @@
+import { redirect } from "next/navigation";
+import { getDefaultSignInView } from "@/lib/utils/auth-helpers/settings";
+import { cookies, type UnsafeUnwrappedCookies } from "next/headers";
+
+export default function SignIn() {
+  const preferredSignInView =
+    (cookies() as unknown as UnsafeUnwrappedCookies).get("preferredSignInView")
+      ?.value || null;
+  const defaultView = getDefaultSignInView(preferredSignInView);
+
+  return redirect(`/signin/${defaultView}`);
+}
